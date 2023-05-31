cbe4d686144b5255e19b362c58d9241bfdb99772
心旅 <130059100+fengjing-1003@users.noreply.github.com>
2023-05-31 15:29:22 +0800

    修改线程安全BUG

--- star-biz/src/main/java/com/xingkong1983/star/biz/BizTool.java
+++ star-biz/src/main/java/com/xingkong1983/star/biz/BizTool.java
@@ -1,5 +1,6 @@
 package com.xingkong1983.star.biz;
 
+import com.xingkong1983.star.biz.vo.BizCode;
 import com.xingkong1983.star.biz.vo.BizResponseVo;
 import com.xingkong1983.star.core.tool.OsTool;
 
@@ -14,23 +15,23 @@ public class BizTool {
 	// ==================================================
 	// 响应成功常量定义
 	// ==================================================
-	public final static BizResponseVo OK = new BizResponseVo(200);
-	public final static BizResponseVo OK_LOGIN = new BizResponseVo(2000);
-	public final static BizResponseVo OK_LOGOUT = new BizResponseVo(2001);
-	public final static BizResponseVo OK_FORM = new BizResponseVo(2002);
-	public final static BizResponseVo OK_UPLOAD = new BizResponseVo(2003);
+	public final static BizCode OK = new BizCode(200);
+	public final static BizCode OK_LOGIN = new BizCode(2000);
+	public final static BizCode OK_LOGOUT = new BizCode(2001);
+	public final static BizCode OK_FORM = new BizCode(2002);
+	public final static BizCode OK_UPLOAD = new BizCode(2003);
 
 	// ==================================================
 	// 响应错误常量定义
 	// ==================================================
-	public final static BizResponseVo ERR = new BizResponseVo(500);
-	public final static BizResponseVo ERR_NOLOGIN = new BizResponseVo(5000);
-	public final static BizResponseVo ERR_LOGIN = new BizResponseVo(5001);
-	public final static BizResponseVo ERR_LOGOUT = new BizResponseVo(5002);
-	public final static BizResponseVo ERR_FORM = new BizResponseVo(5003);
-	public final static BizResponseVo ERR_SMS_CODE = new BizResponseVo(5004);
-	public final static BizResponseVo ERR_UPLOAD = new BizResponseVo(5005);
-	public final static BizResponseVo ERR_NO_COMPANY = new BizResponseVo(5006);
+	public final static BizCode ERR = new BizCode(500);
+	public final static BizCode ERR_NOLOGIN = new BizCode(5000);
+	public final static BizCode ERR_LOGIN = new BizCode(5001);
+	public final static BizCode ERR_LOGOUT = new BizCode(5002);
+	public final static BizCode ERR_FORM = new BizCode(5003);
+	public final static BizCode ERR_SMS_CODE = new BizCode(5004);
+	public final static BizCode ERR_UPLOAD = new BizCode(5005);
+	public final static BizCode ERR_NO_COMPANY = new BizCode(5006);
 
 	// ==================================================
 	// 断言定义
@@ -42,7 +43,6 @@ public class BizTool {
 	public final static BizAssert ASSERT_REDIS = new BizAssert(7002);
 	public final static BizAssert ASSERT_PERSSION = new BizAssert(7003);
 	public final static BizAssert ASSERT_FILE = new BizAssert(7004);
-
 
 	public static BizResponseVo success(String message) {
 		return success(message, null);
@@ -65,7 +65,6 @@ public class BizTool {
 	 * @return 响应对象
 	 */
 	public static BizResponseVo error(String message) {
-
 		return error(message, null);
 	}
 
