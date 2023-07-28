330527bd83ef2ec7cd139533858ee7a90c3e2a62
epat <2507483326@qq.com>
2023-07-28 11:31:06 +0800

    修改 bizException

--- star-biz/src/main/java/com/xingkong1983/star/biz/BizException.java
+++ star-biz/src/main/java/com/xingkong1983/star/biz/BizException.java
@@ -1,1 +1,81 @@
-package com.xingkong1983.star.biz;import lombok.Data;import lombok.EqualsAndHashCode;@Data@EqualsAndHashCode(callSuper = false)public class BizException extends RuntimeException {	private static final long serialVersionUID = 1L;	/**	 * 错误编码	 */	private int code;	/**	 * 操作信息	 */	private String message;	/**	 * 错误日志	 */	private String log;	/**	 * 数据	 */	private Object data;	/**	 * 构造函数	 * @param cause 异常	 */	public BizException(Throwable cause) {		super(cause);		this.code = 500;		this.message = cause.getMessage();	}	/**	 * 构造函数	 * @param code 编码	 * @param message 消息	 */	public BizException(int code, String message) {		super(message);		this.code = code;		this.message = message;	}	/**	 * 构造函数	 * @param code 编码	 * @param message 消息	 * @param data 数据	 */	public BizException(int code, String message, Object data) {		super(message);		this.code = code;		this.message = message;		this.data = data;	}	/**	 * 构造函数	 * @param code 编码	 * @param message 消息	 * @param cause 异常	 */	public BizException(int code, String message, Throwable cause) {		super(cause.getMessage(), cause);		this.code = code;	}	/**	 * 构造函数	 * @param code 编码	 * @param message 消息	 * @param cause 异常	 * @param data 数据	 */	public BizException(int code, String message, Throwable cause, Object data) {		super(cause.getMessage(), cause);		this.code = code;		this.message = message;		this.log = cause.getMessage();		this.data = data;	}}
+
+package com.xingkong1983.star.biz;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class BizException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 错误编码
+	 */
+	private int code;
+
+	/**
+	 * 操作信息
+	 */
+	private String message;
+	/**
+	 * 错误日志
+	 */
+	private String log;
+
+	/**
+	 * 数据
+	 */
+	private Object data;
+
+
+
+
+	/**
+	 * 构造函数
+	 * @param message 消息
+	 */
+	public BizException(String message) {
+		super(message);
+		this.code = 500;
+		this.message = message;
+	}
+
+
+
+	/**
+	 * 构造函数
+	 * @param code 编码
+	 * @param message 消息
+	 */
+	public BizException(String message, Object data) {
+		super(message);
+		this.code = 500;
+		this.message = message;
+		this.data = data;
+	}
+
+	/**
+	 * 构造函数
+	 * @param code 编码
+	 * @param message 消息
+	 */
+	public BizException(int code, String message) {
+		super(message);
+		this.code = code;
+		this.message = message;
+	}
+
+	/**
+	 * 构造函数
+	 * @param code 编码
+	 * @param message 消息
+	 * @param data 数据
+	 */
+	public BizException(int code, String message, Object data) {
+		super(message);
+		this.code = code;
+		this.message = message;
+		this.data = data;
+	}
+}
